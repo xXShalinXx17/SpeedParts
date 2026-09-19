de205fc304c10abd82c84c9c006297b72a65909c
mtaisa342 <matiassolis0104@gmail.com>
2026-09-18 21:33:18 -0300

    Update delimiter_de_almacenes.sql

--- delimiter_de_almacenes.sql
+++ delimiter_de_almacenes.sql
@@ -1,19 +1,37 @@
-USE Almacenes;
 DELIMITER //
 
-CREATE PROCEDURE InsertarAlmacen(
-    IN p_categoria_id INT,
-    IN p_nombre VARCHAR(150),
-    IN p_capacidad INT,
-    IN p_dirección VARCHAR(200)
+CREATE PROCEDURE ConsultarStockSeguro(
+    IN p_ID_usuario BIGINT
 )
 BEGIN
-    INSERT INTO almacen (Categoria_id, nombre, capacidad_MAX, direccion)
-    VALUES (p_categoria_id, p_nombre, p_capacidad, p_direccion);
+    DECLARE v_id_rol INT;
+
+    SELECT ID_rol INTO v_id_rol 
+    FROM CLIENTE.Usuario 
+    WHERE ID_usuario = p_ID_usuario;
+
+    IF v_id_rol IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error de Acceso: El usuario especificado no existe en el sistema.';
+        
+    ELSEIF v_id_rol = 3 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error de Privilegios: Acceso denegado. Los clientes no pueden consultar el stock de almacenes.';
+        
+    ELSE
+        SELECT 
+            A.nombre AS Nombre_Almacen,
+            R.codigo_sku AS Codigo_SKU,
+            R.nombre AS Nombre_Repuesto,
+            R.tipo AS Tipo_Repuesto,
+            S.stock_actual AS Stock_Disponible,
+            S.stock_minimo AS Alerta_Minimo
+        FROM Almacenes.stock_de_repuestos S
+        INNER JOIN Almacenes.almacen A ON S.ID_almacen = A.ID_almacen
+        INNER JOIN Almacenes.Repuestos R ON S.id_repuestos = R.id_repuestos
+        ORDER BY A.nombre ASC, S.stock_actual DESC;
+        
+    END IF;
 END //
 
 DELIMITER ;
-INSERT INTO Categoria (Categoria_id, nombre) VALUES (10, 'Electrónica');
-
-CALL InsertarAlmacen(10, 'Almacén Central Norte', 5000, 'Av. Principal 123');
-CALL InsertarAlmacen(10, 'Almacén Secundario Sur', 2500, 'Calle Lateral 456');
